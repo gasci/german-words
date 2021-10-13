3dee8de7624f4c6613da58c81da76c11587e3d24
Göktuğ Aşcı <g.asci@hotmail.com>
2021-10-13 21:58:18 +0200

    initiate the flask server

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request, render_template
+import json
+from flask_classful import FlaskView, route
+
+app = Flask(__name__, template_folder='templates')
+
+class VocabView(FlaskView):
+    def __init__(self):
+        with open("assets/vocab.json", "r", encoding="utf-8") as json_file:
+            self.data = json.load(json_file)
+
+        # app config
+        app.config['JSON_AS_ASCII'] = False
+    
+    def index(self):
+        return "This is my German learning tool!"
+
+    def search(self):
+        word = request.args.get("word")
+        
+        # increase the request count by one
+        self.data[word]["request_count"] += 1
+
+        print(self.data)
+
+        with open("assets/vocab.json", "w", encoding="utf-8") as jsonFile:
+            json.dump(self.data, jsonFile, ensure_ascii=False)
+            response = self.data[word]
+
+        
+        return render_template('main.html', **response) #
+
+        #return jsonify(response=response)
+
+VocabView.register(app)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0')
---
+++ run.py
@@ -0,0 +1,5 @@
+import os
+
+code = "FLASK_APP=app.py FLASK_ENV=development flask run --port 8080"
+
+os.system(code)
