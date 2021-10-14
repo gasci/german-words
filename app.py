af9b2ce48fc4d6bbd58e96a6d06c75fc30fc9d94
Göktuğ Aşcı <g.asci@hotmail.com>
2021-10-14 22:20:47 +0200

    templates push

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect
 import json
-from flask_classful import FlaskView, route
+from flask_classful import FlaskView
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__, template_folder='templates')
+
+# HOME = os.environ['CLUSTER_USERNAME']
+# CLUSTER_PASSWORD = os.environ['CLUSTER_USERNAME']
+
+# print(HOME)
+# print(CLUSTER_PASSWORD)
+
+@app.route("/")
+def starting_url():
+    return redirect("/word")
 
 class WordView(FlaskView):
     def __init__(self):
         with open("assets/vocab.json", "r", encoding="utf-8") as json_file:
             self.data = json.load(json_file)
 
+        # load variables
+        load_dotenv(".env")
         # app config
         app.config['JSON_AS_ASCII'] = False
     
@@ -22,16 +36,16 @@ class WordView(FlaskView):
         word = request.args.get("word")
         
         # increase the request count by one
-        self.data[word]["request_count"] += 1
+        # self.data[word]["request_count"] += 1
 
-        print(self.data)
+        # print(self.data)
 
-        with open("assets/vocab.json", "w", encoding="utf-8") as jsonFile:
-            json.dump(self.data, jsonFile, ensure_ascii=False)
-            response = self.data[word]
+        # with open("assets/vocab.json", "w", encoding="utf-8") as jsonFile:
+        #     json.dump(self.data, jsonFile, ensure_ascii=False)
+        response = self.data[word]
 
         
-        return render_template('word.html', **response) #
+        return render_template('word.html', **response) 
 
         #return jsonify(response=response)
 
